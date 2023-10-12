1917f58de6aba9e4a7c7e053cff77befb959f65b
santhoshcrypto2019 <67100565+santhoshcrypto2019@users.noreply.github.com>
2023-10-12 13:55:56 +0530

    First exe

---
+++ FirstConsoleApp/FirstConsoleApp.cpp
@@ -0,0 +1,17 @@
+//Copyright A.T.Chamillard. All Rights Reserved.
+
+#include <iostream>
+/**
+* Prints something
+* @return Exit Status 
+*/
+
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
